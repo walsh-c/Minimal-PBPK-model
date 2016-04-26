7e68e7928355f359b506ec4eba5d5f8d89ad6a92
ChrisW <chriswalsh181@gmail.com>
2016-04-26 11:27:15 +0100

    Added comments to the Simple PBPK model for verapimil script

--- Simple PBPK model for verapimil (no eliminiation).R
+++ Simple PBPK model for verapimil (no eliminiation).R
@@ -1,7 +1,12 @@
 library(deSolve)
 
+# Set path to the directory containing the files from GitHub
 path <- "~/PhD Work/Minimal PBPK model/Minimal-PBPK-model/"
 
+# Load in the PBPK model
+source(paste(path, "/differentials for PBPK model.R", sep = ""))
+
+# Read in the parameters needed for the model
 drug <- read.csv(paste(path, "Verapimil.csv", sep = ""))
 organism <- read.csv(paste(path, "organism.csv", sep = ""), row.names = 1)
 parameters <- read.csv(paste(path, "parameters.csv", sep = ""), row.names = 1)
@@ -10,8 +15,8 @@ state <- c(C.Vein = 0.00147232, C.Artery = 0, C.Liver = 0, C.Kidney = 0, C.Gut =
            C.Bone = 0)
 time <- seq(0, 24, by = 0.001)
 
-source(paste(path, "/differentials for PBPK model.R", sep = ""))
-
+# Run the PBPK model using the default solver.
 out <- ode(y = state, times = time, func = PBPK, parms = c(drug, organism, parameters))
 
+#Print out the first 6 time points for each compartment
 print(head(out))
